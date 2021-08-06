0e0b9a8233abb61051c0fb962f94983362f44a31
caspari-svg <holguinsebastian9@gmail.com>
2021-08-06 13:07:30 -0400

    Tested frontend webpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+function WelcomeMessage() {
+  return <p>Welcome!</p>
+}
+
 function App() {
   return (
     <div className="App">
@@ -9,9 +13,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <WelcomeMessage />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://youtube.com"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -19,7 +24,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
