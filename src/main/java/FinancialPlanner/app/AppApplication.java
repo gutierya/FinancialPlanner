fdf9dfd9b9b2050ffe8a5fa03cc3933ee9459d1a
gutierya <52222018+gutierya@users.noreply.github.com>
2021-08-10 23:45:59 -0400

    fixed goal controller link

--- src/main/java/FinancialPlanner/app/AppApplication.java
+++ src/main/java/FinancialPlanner/app/AppApplication.java
@@ -1,13 +1,39 @@
 package FinancialPlanner.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
+
+	@Autowired
+	private Environment env;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				String urls = env.getProperty("cors.urls");
+				CorsRegistration reg = registry.addMapping("/api/**");
+				for(String url: urls.split(",")) {
+					reg.allowedOrigins(url);
+				}
+			}
+		};
+	}
 }
--- src/main/java/FinancialPlanner/app/Models/User.java
+++ src/main/java/FinancialPlanner/app/Models/User.java
@@ -71,7 +71,7 @@ public class User {
     }
 
     public void setCompletedGoals(List<Goal> completedGoals) {
-        this.completedGoals = completedGoals;
+        this.completedGoals = completedGoals;}
 
     @Override
     public String toString() {
