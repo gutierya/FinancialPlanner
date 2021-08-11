50a207770216b659a13b59dc8b517ebdbcf4a41b
caspari-svg <holguinsebastian9@gmail.com>
2021-08-10 21:42:10 -0400

    Added Sign-In page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,18 +2,12 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 
-import { Link } from "react-router-dom";
-
-import Button from "@material-ui/core/Button";
-import GoalView from "./components/GoalView";
-
 class Home extends Component {
   render() {
     return (
       <div>
         <Navbar />
 
-        <GoalView />
       </div>
     );
   }
