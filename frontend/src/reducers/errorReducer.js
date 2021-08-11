21da7795a809c7b4ac4093d700aab3350aa645da
gutierya <52222018+gutierya@users.noreply.github.com>
2021-08-11 11:49:33 -0400

    backend optimize imports

--- frontend/src/reducers/errorReducer.js
+++ frontend/src/reducers/errorReducer.js
@@ -3,6 +3,7 @@ import { GET_ERRORS } from "../actions/Types";
 /* with errors we always have an initial state */
 const initialState = {};
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_ERRORS:
